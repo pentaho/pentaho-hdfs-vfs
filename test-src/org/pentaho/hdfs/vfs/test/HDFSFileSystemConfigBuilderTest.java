0b2f241c1230144bd58aa35a6501316c46df756f
Benjamin Morrise <bmorrise@pentaho.com>
2015-07-01 09:12:28 -0400

    [BACKLOG-3756] Update projects with the pentaho vfs version to version 2

--- test-src/org/pentaho/hdfs/vfs/test/HDFSFileSystemConfigBuilderTest.java
+++ test-src/org/pentaho/hdfs/vfs/test/HDFSFileSystemConfigBuilderTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.apache.commons.vfs.FileSystemOptions;
+import org.apache.commons.vfs2.FileSystemOptions;
 import org.junit.Test;
 import org.pentaho.hdfs.vfs.HDFSFileSystemConfigBuilder;
 
